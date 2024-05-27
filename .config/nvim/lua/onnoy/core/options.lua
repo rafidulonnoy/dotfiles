4e41c961a65e7b95700e3cd27d5f705afdd9e779
rafidulonnoy <rafidulonnoy@gmail.com>
2024-05-27 23:57:00 +0600

    added GNU stow to manage dotfiles cleared some bugs and refined all the config files added zshrc file

--- .config/nvim/lua/onnoy/core/options.lua
+++ .config/nvim/lua/onnoy/core/options.lua
@@ -11,13 +11,26 @@ opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copyindent from current line when starting new one
 
-opt.wrap = false
+opt.wrap = true
 
 --search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
+-- Save undo history
+opt.undofile = true
 opt.cursorline = true
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
 
 -- turn on terguicolors for tokyonight colorscheme to work
 opt.termguicolors = true
@@ -30,7 +43,9 @@ opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or 
 -- clipboard
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
+-- Save undo history
+opt.undofile = true
+
 -- split windows
 opt.splitright = true
 opt.splitbelow = true
-
